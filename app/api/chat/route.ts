33a9003610f3110792b835fadd94047b42b6ed39
Victor Jiang <victorjchess9@gmail.com>
2026-04-27 20:00:45 -0400

    changed model to 2.5

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -110,7 +110,7 @@ export async function POST(req: Request) {
 
   const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GENERATIVE_AI_KEY);
   const model = genAI.getGenerativeModel({
-    model: "gemini-2.0-flash-lite",
+    model: "gemini-2.5-flash-lite",
     systemInstruction,
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     tools: [{ functionDeclarations: functionDeclarations as any }],
